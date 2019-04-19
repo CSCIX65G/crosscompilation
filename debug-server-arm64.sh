de5d03ab1970d8668f26c2da705126a9c1e96a81
Van Simmons <van.simmons@computecycles.com>
2019-04-19 07:03:27 -0400

    fix arm script

--- debug-server-arm64.sh
+++ debug-server-arm64.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-docker pull cscix65g/swift-runtime:amd64-latest
+docker pull cscix65g/swift-runtime:arm64-latest
 if [ ! "$(docker ps --all -q -f name=swift_runtime)" ]; then
     echo "Launching swift_runtime"
-    docker run --name swift_runtime -d cscix65g/swift-runtime:amd64-latest
+    docker run --name swift_runtime -d cscix65g/swift-runtime:arm64-latest
     docker logs swift_runtime
 fi
 
-docker pull cscix65g/lldb-server:amd64-latest
+docker pull cscix65g/lldb-server:arm64-latest
 if [ ! "$(docker ps --all -q -f name=lldb_server)" ]; then
     echo "Launching lldb-server"
-    docker run -d --rm --privileged --name lldb_server --network host -v `pwd`/tmp:/debug --volumes-from swift_runtime  cscix65g/lldb-server:amd64-latest
+    docker run -d --rm --privileged --name lldb_server --network host -v `pwd`/tmp:/debug --volumes-from swift_runtime  cscix65g/lldb-server:arm64-latest
     docker logs lldb_server
 fi
