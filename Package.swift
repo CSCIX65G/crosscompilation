83cd1f7326654d2f4712b9639820b91a1d72ebd1
Van Simmons <rvsrvs@gmail.com>
2019-04-21 07:00:43 -0400

    updating for arm64

--- Package.swift
+++ Package.swift
@@ -1,38 +1,48 @@
-// swift-tools-version:4.1
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
+let dependencies: [PackageDescription.Package.Dependency] = [
+    .package(url: "https://github.com/CSCIX65G/smoke-framework.git", .branch("swift5")),
+    .package(url: "https://github.com/AlwaysRightInstitute/Shell.git", from: "0.1.4"),
+    .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .branch("master")),
+    .package(url: "https://github.com/CSCIX65G/SwiftyLinkerKit.git", .branch("swift5"))
+]
+
+let serverTargetDependencies: [Target.Dependency] = ["Service", "EchoService", "HeliumLogger", "Shell", "SwiftyLinkerKit"]
+
 let package = Package(
     name: "echoserver",
     products: [
-      .executable(name: "echoserver",
-                  targets: [
-                    "Service",
-                    "EchoService",
-                    "Server",
-		  ]
-      ),
-      .library(name: "Service", targets: ["Service"]),
-      .library(name: "EchoService", targets: ["EchoService", "Service"]),
+        .executable(
+            name: "echoserver",
+            targets: [
+                "Service",
+                "EchoService",
+                "Server",
+            ]
+        ),
+        .library(name: "Service", targets: ["Service"]),
+        .library(name: "EchoService", targets: ["EchoService", "Service"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/amzn/smoke-framework.git", .branch("master")),
-	.package(url: "https://github.com/AlwaysRightInstitute/Shell.git", from: "0.1.4"),
-        .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .branch("master"))
-    ],
+    dependencies: dependencies,
     targets: [
         .target(
             name: "Server",
-            dependencies: ["Service", "EchoService", "HeliumLogger", "Shell"]),
+            dependencies: serverTargetDependencies
+        ),
         .target(
             name: "EchoService",
-            dependencies: ["Service", "SmokeOperations", "SmokeHTTP1", "SmokeOperationsHTTP1"]),
+            dependencies: ["Service", "SmokeOperations", "SmokeHTTP1", "SmokeOperationsHTTP1"]
+        ),
         .target(
             name: "Service",
-            dependencies: ["SmokeOperations", "SmokeHTTP1", "SmokeOperationsHTTP1"]),
+            dependencies: ["SmokeOperations", "SmokeHTTP1", "SmokeOperationsHTTP1"]
+        ),
         .testTarget(
             name: "ServerTests",
-            dependencies: ["Server"]),
+            dependencies: ["Server"]
+        ),
     ]
 )
--- build-arm64.sh
+++ build-arm64.sh
@@ -1,2 +1,2 @@
-swift build --destination /Library/Developer/Destinations/arm64-ubuntu-bionic.json
+swift build -Xswiftc -Darm64 --destination /Library/Developer/Destinations/arm64-ubuntu-bionic.json
 docker build --file ./Dockerfile-arm64 --tag echoserver:arm64-latest .
